e14c4f13f763cfbb771724fd18a6d5c395652298
Maksim Minakov <maksim.minakov.83@mail.ru>
2026-05-31 10:51:09 +0300

    Initial Commit

---
+++ TodolistReactSwiftUI/TodolistReactSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodolistReactSwiftUIApp.swift
+//  TodolistReactSwiftUI
+//
+//  Created by Максим Минаков on 31.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct TodolistReactSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
